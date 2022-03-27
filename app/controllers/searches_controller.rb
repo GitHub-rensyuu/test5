e1206a8287333fa74502468b1ecd40d110912550
EC2 Default User <ec2-user@ip-172-31-33-222.ec2.internal>
2022-03-27 19:27:37 +0900

    eachnilのエラー修正中

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,50 @@
+class SearchesController < ApplicationController
+  # before_action :authenticate_user!
+
+  def search
+    # viewのform_tagにて
+    # 選択したmodelの値を@modelに代入。
+    @model = params["model"]
+    # 選択した検索方法の値を@methodに代入。
+    @method = params["method"]
+    # 検索ワードを@contentに代入。
+    @content = params["content"]
+    # @model, @content, @methodを代入した、
+    # search_forを@recordsに代入。
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+  def search_for(model, content, method)
+    # 選択したモデルがuserだったら
+    if model == 'user'
+      # 選択した検索方法がが完全一致だったら
+      if method == 'perfect_match'
+        User.where(name: content)
+      # 選択した検索方法がが前方一致だったら
+      elsif method == "forward_match"
+        User.where("name LIKE?", '%'+content+'%')
+      # 選択した検索方法がが後方一致だったら
+      elsif method == "backward_match"
+        User.where("name LIKE?", '%'+content+'%')
+      # 選択した検索方法がが部分一致だったら
+      elsif method == "partial_match"
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+
+    # 選択したモデルbookだったら
+    elsif model == 'book'
+      if method == 'perfect_match'
+        Book.where(name: content)
+      elsif method == "forward_match"
+        Book.where("name LIKE?", '%'+content+'%')
+      elsif method == "backward_match"
+        Book.where("name LIKE?", '%'+content+'%')
+      elsif method == "partial_match"
+        Book.where('name LIKE ?', '%'+content+'%')
+      end
+    end
+
+  end
+
+end
