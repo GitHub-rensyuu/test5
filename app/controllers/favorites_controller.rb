760163653ab335a77fcde92a333824eeed0e618b
EC2 Default User <ec2-user@ip-172-31-33-222.ec2.internal>
2022-03-28 11:53:26 +0900

    リロードしないと更新されないエラー修正中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  # リダイレクト先を削除、
+  # リダイレクト先がない、かつJavaScriptリクエストという状況になり、
+  # createアクション実行後は、create.js.erbファイルを、
+  # destroyアクション実行後はdestroy.js.erbファイルを探すようになる
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
