edf827527f0c30a7b8fd91cbf25968da8536f2a8
RT <u7def37decdwd@gmail.com>
2022-04-09 12:21:44 +0000

    復習

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_061730) do
+ActiveRecord::Schema.define(version: 2022_04_06_083659) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_061730) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "rate"
+    t.string "category"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -113,7 +114,8 @@ ActiveRecord::Schema.define(version: 2022_04_04_061730) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at", precision:
+    6, null: false
   end
 
   create_table "users", force: :cascade do |t|
